6f449433a65cf68ccf1a2b11de7a50710d2ccf89
Gruruya <greytest@disroot.org>
2023-04-16 10:41:36 -0400

    Correct path to nimble file

--- src/niomo.nim
+++ src/niomo.nim
@@ -435,7 +435,7 @@ when isMainModule:
   import pkg/[cligen/argcvt]
   # taken from c-blake "https://github.com/c-blake/cligen/issues/212#issuecomment-1167777874"
   include cligen/mergeCfgEnvMulMul
-  const nimbleFile = staticRead("niomo.nimble")
+  const nimbleFile = staticRead(currentSourcePath().parentDir /../ "niomo.nimble")
   clCfg.version = nimbleFile.fromNimble("version")
   proc argParse[T](dst: var Option[T], dfl: Option[T],
                    a: var ArgcvtParams): bool =
