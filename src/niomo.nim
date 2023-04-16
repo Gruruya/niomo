ed52d88f2b1906b14b773bd0462d7baef66d14fc
Gruruya <greytest@disroot.org>
2023-04-16 10:36:45 -0400

    Add version information and bump

--- src/niomo.nim
+++ src/niomo.nim
@@ -435,6 +435,8 @@ when isMainModule:
   import pkg/[cligen/argcvt]
   # taken from c-blake "https://github.com/c-blake/cligen/issues/212#issuecomment-1167777874"
   include cligen/mergeCfgEnvMulMul
+  const nimbleFile = staticRead("niomo.nimble")
+  clCfg.version = nimbleFile.fromNimble("version")
   proc argParse[T](dst: var Option[T], dfl: Option[T],
                    a: var ArgcvtParams): bool =
       var uw: T           # An unwrapped value
