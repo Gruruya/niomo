d20ae1a00e332c1375999036a4729ac49448e0a7
Gruruya <gruruya.chi4c@slmails.com>
2023-05-25 06:17:03 -0400

    correct `truncPubkey` typing

--- src/niomo/alias.nim
+++ src/niomo/alias.nim
@@ -69,7 +69,7 @@ proc next(self: Lsfr): uint64 {.raises: [].} =
   self.data = bitor(self.data shl 1, bit)
   result = self.data
 
-func truncPubkey(pubkey: SkPublicKey | SkXOnlyPublicKey): uint64 =
+func truncPubkey(pubkey: SkPublicKey): uint64 =
   let rawKey = pubkey.toRaw
   fromBytesBE(uint64, rawKey[25..32])
 
